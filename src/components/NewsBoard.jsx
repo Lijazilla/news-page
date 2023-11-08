167d127b10eb9a4711177fdf5890920c6aa78ebf
alex <alejandro-1986-jf@hotmail.com>
2023-11-08 11:41:59 -0600

    se agregó un try catch

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -2,24 +2,49 @@ import { useEffect } from "react";
 import { useState } from "react"
 import NewsItem from "./NewsItem";
 
-
-const NewsBoard = ({category}) => {
-
+const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null); // Nuevo estado para manejar errores
 
-  useEffect(()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-    fetch(url).then(Response => Response.json()).then(data=> setArticles(data.articles))
-  }, [category])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setArticles(data.articles);
+      } catch (error) {
+        // Captura cualquier error y muestra un mensaje de error
+        setError("Error al cargar noticias. Por favor, inténtalo de nuevo más tarde.");
+      }
+    };
+
+    fetchData();
+  }, [category]);
 
   return (
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span> </h2>
-      {articles.map((news, index)=> {
-          return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
-      })}
+      {error ? (
+        // Muestra un mensaje de error si ocurrió un error en la solicitud
+        <p>{error}</p>
+      ) : (
+        // Muestra las noticias si no hubo errores
+        articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
